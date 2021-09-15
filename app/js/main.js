a2615bd48952f62400b65fbca87671f9e28be538
Burkhardt30 <burkhardt30tto@gmail.com>
2021-09-15 16:42:54 +0500

    added header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,25 @@
-$(function(){
+$(function () {
+
+  $('.header__slider').slick({
+    vertical: true,
+    arrows: false,
+    slidesToShow: 4,
+  });
+  $('.dot-0').on('click', function(e){
+    e.preventDefault()
+    $('.header__slider').slick('goTo',3);
+  });
+  $('.dot-1').on('click', function(e){
+    e.preventDefault()
+    $('.header__slider').slick('goTo',2);
+  });
+  $('.dot-2').on('click', function(e){
+    e.preventDefault()
+    $('.header__slider').slick('goTo',1);
+  });
+  $('.dot-3').on('click', function(e){
+    e.preventDefault()
+    $('.header__slider').slick('goTo',0);
+  });
 
 });
